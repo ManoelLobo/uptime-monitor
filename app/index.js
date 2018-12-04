8b277ed27b57ae55391dcf7eae2b4960dbd5f026
Manoel Lobo <manoel.lobo@gmail.com>
2018-12-04 10:27:48 -0300

    Parse request paths

--- app/index.js
+++ app/index.js
@@ -1,7 +1,19 @@
 const http = require('http');
+const url = require('url');
 
 const server = http.createServer((req, res) => {
+  // parse url
+  const parsedUrl = url.parse(req.url, true);
+
+  // get path
+  const path = parsedUrl.pathname;
+  const trimmedPath = path.replace(/^\/+|\/+$/g, '');
+
+  // send response
   res.end('I hear you!\n');
+
+  // log
+  console.log('Request received on path ' + trimmedPath);
 });
 
 server.listen(3000, () => {
