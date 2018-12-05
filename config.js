87d7efd33f1af46e24b8f96be7a19784c90baba4
Manoel Lobo <manoel.lobo@gmail.com>
2018-12-05 07:30:16 -0300

    Helpers lib
    Hash

--- config.js
+++ config.js
@@ -4,11 +4,13 @@ environments.staging = {
   httpPort: 3000,
   httpsPort: 3001,
   envName: 'staging',
+  hashingSecret: 'SECRET!!!',
 };
 environments.production = {
   httpPort: 5000,
   httpsPort: 5001,
   envName: 'production',
+  hashingSecret: 'SECRET!!!',
 };
 
 const currentEnv =
